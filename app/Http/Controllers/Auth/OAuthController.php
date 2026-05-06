43f380c635bf6ceafcf00f9e98b321923f08307c
bangik <aldi.novaldi.frmnsyah@gmail.com>
2026-05-06 15:05:30 +0700

    update OAuthController to use config for OAuth client credentials; modify Dockerfile.staging to specify network for container

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -20,10 +20,10 @@ class OAuthController extends Controller
 
     public function callback(Request $request)
     {
-        $response = Http::withoutVerifying()->asForm()->post(config('app.super_app_url') . '/oauth/token', [
+        $response = Http::asForm()->post(config('app.super_app_url_internal') . '/oauth/token', [
             'grant_type' => 'authorization_code',
-            'client_id' => env('OAUTH_CLIENT_ID'),
-            'client_secret' => env('OAUTH_CLIENT_SECRET'),
+            'client_id' => config('auth.oauth.client_id'),
+            'client_secret' => config('auth.oauth.client_secret'),
             'redirect_uri' => route('auth.callback'),
             'code' => $request->code,
         ]);
